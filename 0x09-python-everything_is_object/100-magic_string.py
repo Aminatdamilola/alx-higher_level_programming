856e5021224ff35e398ce5896d232a4d15f91db8
aminatdamilola <aminatdamilola50@gmail.com>
2023-02-03 01:50:45 -0800

    everything is object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def magic_string():
+
+    setattr(magic_string, "i", +1)
+    getattr(magic_string, "i", -1)
+    return "BestSchool" + ", BestSchool" *
+getattr(magic_string, "i", 0)
