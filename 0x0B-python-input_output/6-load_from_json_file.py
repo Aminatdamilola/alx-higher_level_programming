bf5fad39dee0416cc2ca158e69e684f41bcf6993
aminatdamilola <aminatdamilola50@gmail.com>
2023-02-08 11:35:47 -0800

    Task 9

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,6 +2,7 @@
 """JSON conversion to objects"""
 import json
 
+
 def load_from_json_file(filename):
     """A function that creates an object from a JSON file
     
@@ -10,6 +11,7 @@ def load_from_json_file(filename):
     
     Returns:
         the object.
+    
     """
 
     with open(filename) as f:
