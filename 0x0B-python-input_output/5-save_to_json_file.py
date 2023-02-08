c9071e9247df32ee257146c40edf6cbd7a951160
aminatdamilola <aminatdamilola50@gmail.com>
2023-02-08 04:18:59 -0800

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""JSON conversion to objects"""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """A function that writes an object to a text file using a JSON format
+    Args:
+        my_obj (:obj:): object to be converted
+        filename : file to write into
+    Returns:
+            The object.
+    """
+
+    with open(filename, 'w') as f:
+    json.dump(my_obj, f) 
