c720fd5a42bd3619e6ef255c6bd6317623bf9221
root <aminatdamilola50@gmail.com>
2023-04-12 09:35:58 +0000

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node script.js <number>');
-  process.exit(1);
-}
-
-const x = parseInt(process.argv[2], 10);
+const num = process.argv[2];
+const x = parseInt(num);
 
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
-  process.exit(1);
-}
-
-for (let i = 0; i < x; i++) {
-  console.log('C is fun');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
