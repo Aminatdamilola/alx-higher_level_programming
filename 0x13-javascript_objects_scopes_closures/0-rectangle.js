a6eb72bc99de8da4ed856a78741caa7dee910203
root <aminatdamilola50@gmail.com>
2023-04-13 22:08:54 +0000

    Tadk 1

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
-  }
-}
 
-const emptyRectangle = new Rectangle(0, 0);
+class Rectangle {
+
+}
