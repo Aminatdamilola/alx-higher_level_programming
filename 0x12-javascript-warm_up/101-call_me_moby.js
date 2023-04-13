19229865e2e494253e1f787ce2d82637f45cb149
root <aminatdamilola50@gmail.com>
2023-04-13 10:21:37 +0000

    Task 13

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-function RepeatFunction () {}
+/**function RepeatFunction () {}
 
 RepeatFunction.prototype.execute = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
-}
+}*/
+function (x, theFunction).exports = repeatFunction;
+
