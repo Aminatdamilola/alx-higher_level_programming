f0d786f2aeec17bcc317214548ce1701280b6aec
root <aminatdamilola50@gmail.com>
2023-04-13 07:48:26 +0000

    Task 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-function add(Int1, Int2) {
+function add (Int1, Int2) {
   return Int1 + Int2;
 }
-console.log(add(3, 5));
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+const sum = add(num1, num2);
+console.log(sum);
