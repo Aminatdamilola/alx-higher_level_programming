252386f576e0a4c16ece0c2b037fac0ec59881df
root <aminatdamilola50@gmail.com>
2023-04-14 12:51:44 +0000

    Task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,10 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      return {};
+      // this.name = 'Rectangle';
+      this.width = w;
+      this.height = h;
     }
-    // this.name = 'Rectangle';
-    this.width = w;
-    this.height = h;
   }
 }
-exports.Rectangle = Rectangle;
+module.exports = Rectangle;
