356ab3adee366338ae47db18c2627ed025eab98d
aminatdamilola <aminatdamilola50@gmail.com>
2023-04-18 12:10:14 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Lists all states with a name starting
-with N from the databasehbtn_0e_0_usa"""
+with N (uppper N)from the databasehbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
@@ -11,8 +11,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%"
-                ORDER BY states.id ASC ')
+    cur.execute("""SELECT * FROM states WHERE name LIKE
+                BINARY 'N%' ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
