b6063c9f3a83a28381ae4503c0c698e36ec3afae
aminatdamilola <aminatdamilola50@gmail.com>
2023-04-18 02:57:30 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
 import MySQLdb
 import sys
-if __name__ == '__main__':
-
+"""if __name__ == '__main__':"""
 """mysql_username = sys.argv[1]
 mysql_password = sys.argv[2]
 database_name = sys.argv[3]"""
