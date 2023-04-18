b028a993fc9b7aad6486e051ef686adf4168902e
aminatdamilola <aminatdamilola50@gmail.com>
2023-04-18 03:58:18 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Lists all states from the databasehbtn_0e_0_usa"""
+""" Lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
       
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import sys
 
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
