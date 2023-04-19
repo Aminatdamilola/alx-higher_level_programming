1b09924d764eee909bc76e186592d966c9d6341a
aminatdamilola <aminatdamilola50@gmail.com>
2023-04-19 14:56:55 -0700

    all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Lists all cities from the databasehbtn_0e_0_usa"""
+""" Lists all cities from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC;")
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
