43deafc3047259fc8ddb380adc3fd887bff70ba0
Bakare Aminat <aminatdamilola50@gmail.com>
2023-04-21 21:22:48 -0700

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     states = session.query(State).filter(
              State.name.like("%a%")).order_by(State.id).all()
-for state in states:
-    print("{}: {}".format(state.id, state.name))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
 
 session.close()
