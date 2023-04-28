ee4fe00ba5803ec900c03d83d9ce79c126a77b3d
Bakare Aminat <aminatdamilola50@gmail.com>
2023-04-27 23:58:52 -0700

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c | xargs echo "The size of the response body is {} bytes"
+#!/bin/bash
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | xargs echo "The size of the response body is {} bytes"
+
