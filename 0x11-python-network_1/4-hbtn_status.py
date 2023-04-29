d92521949843e42f7297e455acaca15e87b9f9fa
Bakare Aminat <aminatdamilola50@gmail.com>
2023-04-28 23:09:15 -0700

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 response = requests.get('https://alx-intranet.hbtn.io/status')
 content = response.text
 
-print('Body response:')
-print('\t- type: {}'.format(type(content)))
-print('\t- content: {}'.format(content))
+print("Body response:")
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
