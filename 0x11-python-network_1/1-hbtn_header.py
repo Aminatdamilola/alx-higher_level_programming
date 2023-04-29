ae8e2cd766d0ceb1e0be18639d0e2ac42c145789
Bakare Aminat <aminatdamilola50@gmail.com>
2023-04-28 20:42:59 -0700

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
-import sys
-
+from sys import argv
 url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
+with urllib.request.urlopen(argv[1]) as response:
     headers = response.info()
     x_request_id = headers['X-Request-Id']
 
