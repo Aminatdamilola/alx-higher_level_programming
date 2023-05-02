7400aa8646c9201e8a2952ffd7420f84efee1816
Bakare Aminat <aminatdamilola50@gmail.com>
2023-05-02 16:11:07 -0700

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Script that takes in a URL, sends a request to the URL and displays the
-body of the response."""
+"""Script that takes in a URL, sends a request to the URL
+and displays the body of the response."""
 
-import requests
-import sys
 
-url = sys.argv[1]
+if __name__ == '__main__':
+    from sys import argv
+    from requests import get
 
-response = requests.get(url)
+    url = argv[1]
 
-if response.status_code >= 400:
-    print("Error code: {}".format(response.status_code))
-else:
-    print(response.content.decode('utf-8'))
+    response = get(url)
+    ERR_TXT = 'Error code: {}'
+    status = response.status_code
+    print(ERR_TXT.format(status) if (status >= 400) else response.text)
