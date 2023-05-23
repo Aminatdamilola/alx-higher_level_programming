75ef969c136a4a7ed4e141bbcde52a5c4a2925ac
Bakare Aminat <aminatdamilola50@gmail.com>
2023-05-23 19:26:35 +0000

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,21 +2,8 @@
 
 const fs = require('fs');
 
-function writeFile (filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (error) => {
-    if (error) {
-      console.log('An error occurred while writing to the file:');
-      console.error(error);
-    } else {
-      console.log('File written successfully!');
-    }
-  });
-}
-
-if (process.argv.length < 4) {
-  console.log('Please provide the file path and the string to write as arguments.');
-} else {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeFile(filePath, content);
+try {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, result) { if (err) console.log(err); });
+} catch (err) {
+  console.log(err);
 }
